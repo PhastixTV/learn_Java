1490e66540b68f38c243503f2223570f3805bb01
PhastixTV <philipp.sidler0206@gmail.com>
2023-07-20 13:45:49 +0200

    Kreisberechnung4OptionPane

--- src/main/java/phastixtv/ch/aufgaben/Uebergabe.java
+++
@@ -1,2 +0,0 @@
-package phastixtv.ch;public class uebergabe {
-}
--- src/main/java/phastixtv/ch/aufgaben2/Kreisberechnung4OptionPane.java
+++ src/main/java/phastixtv/ch/aufgaben2/Kreisberechnung4OptionPane.java
@@ -1,2 +1,17 @@
-package phastixtv.ch.aufgaben2;public class Kreisberechnung4OptionPane {
+package phastixtv.ch.aufgaben2;
+
+import javax.swing.*;
+
+public class Kreisberechnung4OptionPane {
+
+    public static void main(String[] args) {
+        var eingabe = JOptionPane.showInputDialog("Geben Sie den Kreisradius ein: ");
+        var radius = Double.parseDouble(eingabe);
+        eingabe = JOptionPane.showInputDialog("Geben sie die Einheit an: ");
+        var einheit = eingabe;
+        var umfang = 2.0 * 3.1415926 * radius;
+        var flaeche = radius * radius * 3.1415926;
+        JOptionPane.showMessageDialog(null, "Umfang: " + umfang + " " + einheit +
+                                        "\nFläche: " + flaeche + " " + einheit + '\u00b2');
+    }
 }

b50d938ff2e3648747b14896625422255a076fb5
PhastixTV <philipp.sidler0206@gmail.com>
2023-09-22 13:10:16 +0200

    I continued learning

--- src/main/java/phastixtv/ch/aufgaben3/VokaltestSwitchcase.java
+++ src/main/java/phastixtv/ch/aufgaben3/VokaltestSwitchcase.java
@@ -8,11 +8,25 @@ public class VokaltestSwitchcase {
         var eingabe = JOptionPane.showInputDialog("Geben Sie einen Buchstaben ein: ");
         var c = eingabe.charAt(0);
         switch (c) {
-            case 'a','e','i','o','u':
+            //Man könnte auch alle einzeln abfragen, aber das wäre unnötige Arbeit. Work smart and not hard |
+            case 'a','e','i','o','u':                                                                // <----
                 JOptionPane.showMessageDialog(null, c + "ist ein Vokal!");
                 break;
             default:
                 JOptionPane.showMessageDialog(null, c + " ist kein Vokal!");
+
+                /* Eine Möglickeit wäre auch
+                    switch(c) {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        JOptionPane.showMessageDialog(null, c + "ist ein Vokal!");
+                        break;
+                     default:
+                        JOptionPane.showMessageDialog(null, c + " ist kein Vokal!");
+                 */
         }
     }
 }
